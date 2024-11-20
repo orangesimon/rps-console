e43c48c3485229dc05f8408d00be19da59d6091a
orangesimon <saimaimeng@Xubuntu.myguest.virtualbox.org>
2024-11-19 23:45:53 -0500

    Add playRound and playGame function to fulfill RPS

--- rps.js
+++ rps.js
@@ -1,15 +1,12 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     switch(num) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
     return "";
 }
@@ -26,9 +23,61 @@ function getHumanChoice() {
     return selection;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        console.log("You tied! ");
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log("You tied! ");
+            return;
+        }
+        switch(humanChoice) {
+            case "rock":
+                switch(computerChoice) {
+                    case "scissors":
+                        console.log("You won! Rock beats scissors.");
+                        humanScore++;
+                        return;
+                    case "paper":
+                        console.log("You lost! Paper beats rock.");
+                        computerScore++;
+                        return;
+                }
+            case "paper":
+                switch(computerChoice) {
+                    case "scissors":
+                        console.log("You lost! Scissors beats paper.");
+                        computerScore++;
+                        return;
+                    case "rock":
+                        console.log("You won! Paper beats rock.");
+                        humanScore++;
+                        return;
+                }
+            case "scissors":
+                switch(computerChoice) {
+                    case "rock":
+                        console.log("You lost! Rock beats scissors.");
+                        computerScore++;
+                        return;
+                    case "paper":
+                        console.log("You won! Scissors beats paper.");
+                        humanScore++;
+                        return;
+                }
+        }
     }
+
     
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();    
+        playRound(humanSelection, computerSelection);
+        console.log("Computer: " + computerScore + "\n You: " + humanScore);
+    }
 }
+
+
